0ef0f9f0076b7b4ac933e07e322e839f0741f6a9
Vineeth Raj <vineethraj49@gmail.com>
2014-01-27 06:15:57 +0530

    Bring in more safety, to prevent warnings.

--- yummy.sh
+++ yummy.sh
@@ -17,7 +17,7 @@ cd build;
 if `git remote -v | grep -Fxq "legacy"`
 then
 #good to go!
-echo "Good to go! patching build"
+echo "Good to go! patching android_build"
 else
 echo "Adding remote legacy for android_build"
 git remote add legacy git@github.com:legaCyMod/android_build.git;
@@ -28,14 +28,28 @@ git merge FETCH_HEAD;
 cd ../;
 
 cd frameworks/av;
+if `git remote -v | grep -Fxq "legacy"`
+then
+#good to go!
+echo "Good to go! patching android_frameworks_av"
+else
+echo "Adding remote legacy for android_frameworks_av"
 git remote add legacy git@github.com:legaCyMod/android_frameworks_av.git;
+fi;
 git fetch legacy cm-11.0;
 git merge FETCH_HEAD;
 
 cd ../../;
 
 cd vendor/cm;
+if `git remote -v | grep -Fxq "legacy"`
+then
+#good to go!
+echo "Good to go! patching android_vendor_cm"
+else
+echo "Adding remote legacy for android_vendor_cm"
 git remote add legacy git@github.com:legaCyMod/android_vendor_cm.git;
+fi;
 git fetch legacy cm-11.0;
 git merge FETCH_HEAD;
 cd ../../;
